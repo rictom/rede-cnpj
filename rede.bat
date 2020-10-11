ea295dc3b8286b9b827d3a3efb429e4a43d1926f
rictom <71139693+rictom@users.noreply.github.com>
2020-10-11 20:40:42 -0300

    Delete rede.bat

--- rede.bat
+++
@@ -1,3 +0,0 @@
-REM remova REM da linha seguinte e acerte o caminho para ativar o ambiente python
-REM call "C:\Users\<usuario>\AppData\Local\Continuum\anaconda3\Scripts\activate.bat"
-python "rede.py"
