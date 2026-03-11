0a57eeb09e1cc9c9e6e1268195562c0e331bd92f
rictom <71139693+rictom@users.noreply.github.com>
2026-03-11 00:09:16 -0300

    atualização da rotina para baixar os arquivos csv

--- rede_cria_tabelas/dados_cnpj_baixa.py
+++ rede_cria_tabelas/dados_cnpj_baixa.py
@@ -1,20 +1,14 @@
 # -*- coding: utf-8 -*-
 """
-
 lista relação de arquivos na página de dados públicos da receita federal
 e faz o download
+Os arquivos estão disponíveis em
 https://www.gov.br/receitafederal/pt-br/assuntos/orientacao-tributaria/cadastros/consultas/dados-publicos-cnpj
-https://dadosabertos.rfb.gov.br/CNPJ/
-http://200.152.38.155/CNPJ/
+Houve alteração de layout da página de download em fev/2026.
+Talvez seja necessário ajustar o valor do parâmetro share_token na função consulta_base_webdap
 """
 from bs4 import BeautifulSoup
-import requests, wget, os, sys, time, glob, parfive
-
-#url = 'http://200.152.38.155/CNPJ/dados_abertos_cnpj/2024-08/' #padrão a partir de agosto/2024
-#url_dados_abertos = 'https://dadosabertos.rfb.gov.br/CNPJ/dados_abertos_cnpj/'
-#url_dados_abertos = 'http://200.152.38.155/CNPJ/dados_abertos_cnpj/'
-#url_dados_abertos = 'https://arquivos.receitafederal.gov.br/cnpj/dados_abertos_cnpj/'
-url_dados_abertos = 'https://arquivos.receitafederal.gov.br/dados/cnpj/dados_abertos_cnpj/'
+import requests, wget, os, sys, time, glob, parfive, re
 
 pasta_zip = r"dados-publicos-zip" #local dos arquivos zipados da Receita
 pasta_cnpj = 'dados-publicos'
@@ -26,7 +20,7 @@ def requisitos():
     if not os.path.isdir(pasta_zip):
         os.mkdir(pasta_zip)
         
-    arquivos_existentes = list(glob.glob(pasta_cnpj +'/*.*')) + list(glob.glob(pasta_zip + '/*.*'))
+    arquivos_existentes = [k for k in glob.glob(pasta_cnpj +'/*.*') if not k.endswith('.txt')] + list(glob.glob(pasta_zip + '/*.zip'))
     if len(arquivos_existentes):
         #eg.msgbox("Este programa baixa arquivos csv.zip de dados abertos da Receita Federal e converte para uso na RedeCNPJ aplicativo.\nIMPORTANTE: Para prosseguir, as pastas 'dados-publicos' e 'dados-publicos-zip', devem estar vazias, senão poderá haver inconsistências (juntar dados de meses distintos).\n",'Criar Bases RedeCNPJ')
         #if eg.ynbox('Deseja apagar os arquivos das pastas ' + pasta_cnpj + ' e ' + pasta_zip + '?\nNÃO SERÁ POSSÍVEL REVERTER!!!!\n' + '\n'.join(arquivos_existentes) + '\nATENÇÃO: SE FOR EXECUTAR APENAS ALGUMA PARTE DO PROGRAMA, NÃO SELECIONE ESTA OPÇÃO, APAGUE MANUALMENTE.','Criar Bases RedeCNPJ', ['SIM-APAGAR', 'NÃO']):
@@ -39,49 +33,83 @@ def requisitos():
             print('Parando... Apague os arquivos ' + pasta_cnpj + ' e ' + pasta_zip +' e tente novamente')
             input('Pressione Enter')
             sys.exit(1)
-    # else:
-    #     eg.msgbox("Este programa baixa arquivos csv.zip de dados abertos da Receita Federal e converte para uso na RedeCNPJ aplicativo.",'Criar Bases RedeCNPJ')
+#.def requisitos
 
-    # if len(glob.glob(os.path.join(pasta_zip,'*.zip'))):
-    #     print(f'Há arquivos zip na pasta {pasta_zip}. Apague ou mova esses arquivos zip e tente novamente')
-    #     input('Pressione Enter')
-    #     sys.exit(1)
-requisitos()
+def consulta_base_webdap(share_token="YggdBLfdninEJX9", base_url="https://arquivos.receitafederal.gov.br/public.php/webdav"):
+    # código adaptado de github.com/caiopizzol/cnpj-data-pipeline/blob/main/downloader.py
+    from xml.etree import ElementTree
+    DAV_NS = {"d": "DAV:"} # WebDAV XML namespace
+    url = base_url + "/"
+    response = requests.request("PROPFIND", url, auth=(share_token, ""), headers={"Depth": "1"}) #, timeout=(self.config.connect_timeout, self.config.read_timeout))
+    response.raise_for_status()
+    root = ElementTree.fromstring(response.content)
+    
+    directories = []
+    for response in root.findall("d:response", DAV_NS):
+        href = response.find("d:href", DAV_NS).text
+        match = re.search(r"(\d{4}-\d{2})/?$", href) # Match YYYY-MM directory pattern from href path
+        if match:
+            directories.append(match.group(1))
+            
+    ultimoAnoMes = directories[-1]
+    #obtem lista de arquivos
+    response = requests.request("PROPFIND", url+ultimoAnoMes+"/", auth=(share_token, ""), headers={"Depth": "1"}) # ,timeout=(self.config.connect_timeout, self.config.read_timeout) )
+    response.raise_for_status()
+    root = ElementTree.fromstring(response.content)
+    
+    files = []
+    for response in root.findall("d:response", DAV_NS):
+        href = response.find("d:href", DAV_NS).text
+        # Extract .zip filenames from href
+        match = re.search(r"/([^/]+\.zip)$", href, re.IGNORECASE)
+        if match:
+            files.append(match.group(1))
+
+    urlBaseArquivosDoMes = f'https://arquivos.receitafederal.gov.br/public.php/dav/files/{share_token}/{ultimoAnoMes}/' #para baixar usando requests sem webdav
+    urlPaginaDownloadMeses = f'https://arquivos.receitafederal.gov.br/index.php/s/{share_token}'
+    #return {'ultimoAnoMes':ultimoAnoMes, 'arquivos':files}
+    return {'anoMes':ultimoAnoMes, 'urlBaseArquivosDoMes':urlBaseArquivosDoMes, 'arquivos':files, 'urlPaginaDownloadMeses':urlPaginaDownloadMeses}
+#.def consulta_base_webdap
+
+def consulta_base(bwebdap=True):
+    #consulta primeiro por webdap, se não der certo abre a página com webdriver no modo headless
+    r = None
+    if bwebdap:
+        try:
+            r = consulta_base_webdap()
+            print('Consulta por webdap')
+        except Exception as e:
+            print(f"Error: {e}")    
+            print('Não conseguiu obter a data da base pelo método webdap...')
+            print('Navegue até a página https://arquivos.receitafederal.gov.br/ e localize a pasta Dados>Cadastros>CNPJ')
+            print('A url da página conterá um código (user ou share_token) após https://arquivos.receitafederal.gov.br/index.php/s/')
+            print('Copie o user e atualize este código.') 
+    return r
+#.def consulta_base
 
 print(time.asctime(), f'Início de {sys.argv[0]}:')
 
-soup_pagina_dados_abertos = BeautifulSoup(requests.get(url_dados_abertos).text, features="lxml")
-try:
-    ultima_referencia = sorted([link.get('href') for link in soup_pagina_dados_abertos.find_all('a') if link.get('href').startswith('20')])[-1]
-except:
-    print('Não encontrou pastas em ' + url_dados_abertos)
+parametrosSite = consulta_base()
+if not parametrosSite:
     r = input('Pressione Enter.')
-    sys.exit(1)
+    sys.exit(1)   
+    
+ultima_referencia = parametrosSite['anoMes']
+urlBaseArquivosDoMes = parametrosSite['urlBaseArquivosDoMes']
+print('Última base disponível:', ultima_referencia)
+requisitos()
 
+lista = [urlBaseArquivosDoMes+arq for arq in parametrosSite['arquivos']]
 
-url = url_dados_abertos + ultima_referencia
-# page = requests.get(url)    
-# data = page.text
-soup = BeautifulSoup(requests.get(url).text, features="lxml")
-lista = []
-print('Relação de Arquivos em ' + url)
-for link in soup.find_all('a'):
-    if str(link.get('href')).endswith('.zip'): 
-        cam = link.get('href')
-        if not cam.startswith('http'):
-            print(url+cam)
-            lista.append(url+cam)
-        else:
-            print(cam)
-            lista.append(cam)
+print('Relação de Arquivos disponíveis:')
+for k in lista:
+    print(k)
 
 if __name__ == '__main__':        
     resp = input(f'Deseja baixar os arquivos acima para a pasta {pasta_zip} (y/n)?')
     if resp.lower()!='y' and resp.lower()!='s':
         sys.exit()
         
-
-
 print(time.asctime(), 'Início do Download dos arquivos...')
 
 if True: #baixa usando parfive, download em paralelo
@@ -91,22 +119,22 @@ if True: #baixa usando parfive, download em paralelo
     for url in lista:
         downloader.enqueue_file(url, path=pasta_zip, filename=os.path.split(url)[1])
     downloader.download()
-else: #baixar sequencial, rotina antiga
-    def bar_progress(current, total, width=80):
-        if total>=2**20:
-            tbytes='Megabytes'
-            unidade = 2**20
-        else:
-            tbytes='kbytes'
-            unidade = 2**10
-        progress_message = f"Baixando: %d%% [%d / %d] {tbytes}" % (current / total * 100, current//unidade, total//unidade)
-        # Don't use print() as it will print in new line every time.
-        sys.stdout.write("\r" + progress_message)
-        sys.stdout.flush()
+# else: #baixar sequencial, rotina antiga
+#     def bar_progress(current, total, width=80):
+#         if total>=2**20:
+#             tbytes='Megabytes'
+#             unidade = 2**20
+#         else:
+#             tbytes='kbytes'
+#             unidade = 2**10
+#         progress_message = f"Baixando: %d%% [%d / %d] {tbytes}" % (current / total * 100, current//unidade, total//unidade)
+#         # Don't use print() as it will print in new line every time.
+#         sys.stdout.write("\r" + progress_message)
+#         sys.stdout.flush()
       
-    for k, url in enumerate(lista):
-        print('\n' + time.asctime() + f' - item {k}: ' + url)
-        wget.download(url, out=os.path.join(pasta_zip, os.path.split(url)[1]), bar=bar_progress)
+#     for k, url in enumerate(lista):
+#         print('\n' + time.asctime() + f' - item {k}: ' + url)
+#         wget.download(url, out=os.path.join(pasta_zip, os.path.split(url)[1]), bar=bar_progress)
 
         
 print('\n\n'+ time.asctime(), f' Finalizou {sys.argv[0]}!!!')
@@ -114,43 +142,3 @@ print(f"Baixou {len(glob.glob(os.path.join(pasta_zip,'*.zip')))} arquivos.")
 if __name__ == '__main__':
     input('Pressione Enter')
 
-#lista dos arquivos (até julho/2024)
-'''
-http://200.152.38.155/CNPJ/Cnaes.zip
-http://200.152.38.155/CNPJ/Empresas0.zip
-http://200.152.38.155/CNPJ/Empresas1.zip
-http://200.152.38.155/CNPJ/Empresas2.zip
-http://200.152.38.155/CNPJ/Empresas3.zip
-http://200.152.38.155/CNPJ/Empresas4.zip
-http://200.152.38.155/CNPJ/Empresas5.zip
-http://200.152.38.155/CNPJ/Empresas6.zip
-http://200.152.38.155/CNPJ/Empresas7.zip
-http://200.152.38.155/CNPJ/Empresas8.zip
-http://200.152.38.155/CNPJ/Empresas9.zip
-http://200.152.38.155/CNPJ/Estabelecimentos0.zip
-http://200.152.38.155/CNPJ/Estabelecimentos1.zip
-http://200.152.38.155/CNPJ/Estabelecimentos2.zip
-http://200.152.38.155/CNPJ/Estabelecimentos3.zip
-http://200.152.38.155/CNPJ/Estabelecimentos4.zip
-http://200.152.38.155/CNPJ/Estabelecimentos5.zip
-http://200.152.38.155/CNPJ/Estabelecimentos6.zip
-http://200.152.38.155/CNPJ/Estabelecimentos7.zip
-http://200.152.38.155/CNPJ/Estabelecimentos8.zip
-http://200.152.38.155/CNPJ/Estabelecimentos9.zip
-http://200.152.38.155/CNPJ/Motivos.zip
-http://200.152.38.155/CNPJ/Municipios.zip
-http://200.152.38.155/CNPJ/Naturezas.zip
-http://200.152.38.155/CNPJ/Paises.zip
-http://200.152.38.155/CNPJ/Qualificacoes.zip
-http://200.152.38.155/CNPJ/Simples.zip
-http://200.152.38.155/CNPJ/Socios0.zip
-http://200.152.38.155/CNPJ/Socios1.zip
-http://200.152.38.155/CNPJ/Socios2.zip
-http://200.152.38.155/CNPJ/Socios3.zip
-http://200.152.38.155/CNPJ/Socios4.zip
-http://200.152.38.155/CNPJ/Socios5.zip
-http://200.152.38.155/CNPJ/Socios6.zip
-http://200.152.38.155/CNPJ/Socios7.zip
-http://200.152.38.155/CNPJ/Socios8.zip
-http://200.152.38.155/CNPJ/Socios9.zip
-'''
